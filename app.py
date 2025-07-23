536bf44da726ef90b1602f3c99e05c0ef8a53420
Nimbus <mahatonikita929@gmail.com>
2025-07-23 17:43:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def generate_password():
+    password = ''
+    if request.method == 'POST':
+        letters = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        numbers = list('0123456789')
+        symbols = list('!#$%&()*+')
+
+        nr_letters = random.randint(8, 10)
+        nr_symbols = random.randint(2, 4)
+        nr_numbers = random.randint(2, 4)
+
+        password_list = (
+            [random.choice(letters) for _ in range(nr_letters)] +
+            [random.choice(symbols) for _ in range(nr_symbols)] +
+            [random.choice(numbers) for _ in range(nr_numbers)]
+        )
+
+        random.shuffle(password_list)
+        password = ''.join(password_list)
+
+    return render_template('index.html', password=password)
+
+if __name__ == '__main__':
+    app.run()
